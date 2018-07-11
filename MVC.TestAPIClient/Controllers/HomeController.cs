4b4a12fca7106266bd54467ee21740869ffe0eb3
Dimitri Dokadze <dimitri@vehicletracking.com>
2018-07-10 20:31:50 -0400

    Finished the initial implementation of an Identity Server and test clients

---
+++ MVC.TestAPIClient/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MVC.TestAPIClient.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MVC.TestAPIClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+
+        public async Task<IActionResult> CallApiUsingUserAccessToken()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = await client.GetStringAsync("http://localhost:5001/identity");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View("Json");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ VTS.AuthServer.TestAPIClient/Program.cs
@@ -0,0 +1,117 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VTS.AuthServer.TestAPIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to run tests");
+            Console.ReadLine();
+
+            var program = new Program();
+
+            var task = program.TestApiAsync();
+
+            Console.WriteLine("Press Enter to run tests using password grant type");
+            Console.ReadLine();
+
+            task = program.TestApiOwnerPasswordAsync();
+
+            Console.ReadLine();
+        }
+
+        private async Task TestApiAsync()
+        {
+            var disco = await DiscoverEndpointsAsync();
+            if (disco != null)
+            {
+                var token = await RequestTokenAsync(disco.TokenEndpoint);
+                if (token != null)
+                {
+                    await CallApiAsync(token.AccessToken);
+                }
+            }
+        }
+
+        private async Task TestApiOwnerPasswordAsync()
+        {
+            var disco = await DiscoverEndpointsAsync();
+            if (disco != null)
+            {
+                var token = await RequestTokenAsync(disco.TokenEndpoint, "alice", "password");
+                if (token != null)
+                {
+                    await CallApiAsync(token.AccessToken);
+                }
+            }
+        }
+
+        private async Task<DiscoveryResponse> DiscoverEndpointsAsync()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return null;
+            }
+
+            return disco;
+        }
+
+        private async Task<TokenResponse> RequestTokenAsync(string tokenEndpoint)
+        {
+            var tokenClient = new TokenClient(tokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return null;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            return tokenResponse;
+        }
+
+        private async Task<TokenResponse> RequestTokenAsync(string tokenEndpoint, string username, string password)
+        {
+            var tokenClient = new TokenClient(tokenEndpoint, "ro.client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync(username, password, "api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return null;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            return tokenResponse;
+        }
+
+        private async Task CallApiAsync(string accessToken)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+        }
+    }
+}
